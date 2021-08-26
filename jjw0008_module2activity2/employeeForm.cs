cde51d6cf4ae77fa36e7426c744f1b768c5501a6
Jared Whaley <jjw0008@auburn.edu>
2021-08-26 17:22:32 -0400

    Corrected a multitude of logic errors, and integrated the employeeList with ListItemView (albeit in a roundabout way).

--- jjw0008_module2activity2/employeeForm.cs
+++ jjw0008_module2activity2/employeeForm.cs
@@ -17,7 +17,6 @@ namespace jjw0008_module2activity2
         public employeeForm()
         {
             InitializeComponent();
-            employeeListView.;
         }
 
         private void addEmployee(object sender, EventArgs e)
@@ -25,7 +24,22 @@ namespace jjw0008_module2activity2
             employeeList.Add(new Employee(
                 name: nameBox.Text, idNumber: Int32.Parse(idBox.Text),
                 department: departmentBox.Text, position: positionBox.Text));
-            employeeListView.Items.Add($"{nameBox.Text},{idBox.Text},{departmentBox.Text},{positionBox.Text}");
+            employeeToListItem(employeeList[employeeList.Count - 1]);
+            //employeeListView.Items.Add(nameBox.Text).
+            //    SubItems.AddRange(new string[] { idBox.Text,
+            //        departmentBox.Text, positionBox.Text });
+        }
+
+        private void employeeToListItem (Employee employee)
+        {
+            String idString = employee.IdNumber.ToString();
+            while (idString.Length < 5)
+            {
+                idString = "0" + idString;
+            }
+            employeeListView.Items.Add(employee.Name).
+                SubItems.AddRange(new string[] {idString,
+                    employee.Department, employee.Position });
         }
     }
 }
