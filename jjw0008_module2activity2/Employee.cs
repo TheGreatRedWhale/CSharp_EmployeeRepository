2477b242df13e86cf0635e4c14eb52c559f0ac95
Jared Whaley <jjw0008@auburn.edu>
2021-08-26 14:15:17 -0400

    Added the "Employee" class.

---
+++ jjw0008_module2activity2/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jjw0008_module2activity2
+{
+    class Employee
+    {
+    }
+}
