0f09a1bc610050bae83b7366c426a04589b2f19f
Jared Whaley <jjw0008@auburn.edu>
2021-08-26 14:31:53 -0400

    Added properties to "Employee" class.

--- jjw0008_module2activity2/Employee.cs
+++ jjw0008_module2activity2/Employee.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace jjw0008_module2activity2
 {
+    /**<summary>
+     * Class <c>Employee</c> models an employee within a given company.
+     * </summary>*/
     class Employee
     {
+        public String Name { get; set; }        /// holds the employee’s name.
+        public int IdNumber { get; set; }       /// holds the employee’s ID number.
+        public String Department { get; set; }  /// holds the name of the department in which the employee works.
+        public String Position { get; set; }    /// holds the employee’s job title.
     }
 }
